7df801464873f2df22e61afe4a953357602fb545
Erik on Endeavour <erik.falor@usu.edu>
2024-02-03 21:38:32 -0700

    Go through both phases of the Pomodoro cycle
    These cycles are deliberately short for testing:
    * 10 seconds working
    * 5 seconds on break

--- pomodoro.py
+++ pomodoro.py
@@ -8,5 +8,13 @@ def countdown(seconds):
         sleep(1.0)
 
 
+def pomodoro(down, up):
+    while True:
+        print("Back to work! (Ctrl-C to quit)")
+        countdown(down)
+        print("It's time for a break! (Ctrl-C to quit)")
+        countdown(up)
+
+
 if __name__ == "__main__":
-    countdown(100)
+    pomodoro(10, 5)
