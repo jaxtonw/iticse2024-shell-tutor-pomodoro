4d4df407651f88afe60f2766289f56a75983cb38
Erik on Endeavour <erik.falor@usu.edu>
2024-02-03 23:02:51 -0700

    Display countdown using FIGlet characters
    There is a bug: the "Press ENTER" prompt isn't entirely cleaned up.
    My hacky \r fix has failed me.
    Back to the drawing board!

--- pomodoro.py
+++ pomodoro.py
@@ -3,6 +3,102 @@ from time import sleep
 
 DELAY = 0.005
 
+FIGLET = {
+        "0": ["  ___  ",
+              " / _ \\ ",
+              "| | | |",
+              "| |_| |",
+              " \\___/ ",
+              "       "],
+        "1": [" _ ",
+              "/ |",
+              "| |",
+              "| |",
+              "|_|",
+              "   "],
+        "2": [" ____  ",
+              "|___ \\ ",
+              "  __) |",
+              " / __/ ",
+              "|_____|",
+              "       "],
+        "3": [" _____ ",
+              "|___ / ",
+              "  |_ \\ ",
+              " ___) |",
+              "|____/ ",
+              "       "],
+        "4": [" _  _   ",
+              "| || |  ",
+              "| || |_ ",
+              "|__   _|",
+              "   |_|  ",
+              "        "],
+        "5": [" ____  ",
+              "| ___| ",
+              "|___ \\ ",
+              " ___) |",
+              "|____/ ",
+              "       "],
+        "6": ["  __   ",
+              " / /_  ",
+              "| '_ \\ ",
+              "| (_) |",
+              " \\___/ ",
+              "       " ],
+        "7": [" _____ ",
+              "|___  |",
+              "   / / ",
+              "  / /  ",
+              " /_/   ",
+              "       "],
+        "8": ["  ___  ",
+              " ( _ ) ",
+              " / _ \\ ",
+              "| (_) |",
+              " \\___/ ",
+              "       "],
+        "9": ["  ___  ",
+              " / _ \\ ",
+              "| (_) |",
+              " \\__, |",
+              "   /_/ ",
+              "       "],
+        ":": ["   ",
+              " _ ",
+              "(_)",
+              " _ ",
+              "(_)",
+              "   "],
+        }
+
+HOME = "\x1b[H"
+
+
+def timestamp_to_figlet(timestr: str) -> str:
+    digits = []
+    for char in timestr:
+        if char not in FIGLET:
+            raise ValueError(f"{char} is neither a digit nor a colon")
+        digits.append(FIGLET[char])
+
+    if not digits:
+        return "\n"
+
+    output = []
+    for i in range(len(digits[0])):
+        row = []
+        for d in digits:
+            row.append(d[i])
+        output.append("".join(row))
+    return HOME + "\x1b[K\n".join(output)
+
+
+def clear():
+    """clear the screen and return cursor to home position"""
+    print(end="\x1b[H\x1b[J", flush=True)
+
+
 def press_enter(message):
     input(f"\a{message} Press [ENTER] to start cycle, Ctrl-C to quit")
 
@@ -14,9 +110,9 @@ def seconds_to_timestamp(seconds):
     minutes %= 60
 
     if hours > 0:
-        return f"{hours:02}:{minutes:02}:{seconds:02}"
+        return timestamp_to_figlet(f"{hours:02}:{minutes:02}:{seconds:02}")
     else:
-        return f"{minutes:02}:{seconds:02}"
+        return timestamp_to_figlet(f"{minutes:02}:{seconds:02}")
 
 
 def countdown(seconds):
@@ -27,6 +123,7 @@ def countdown(seconds):
 
 
 def pomodoro(down, up):
+    clear()
     cycle = 0
     while True:
         try:
